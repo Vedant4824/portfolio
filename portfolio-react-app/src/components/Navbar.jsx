25718c5b5c36aa658d7858a1c656ba5e30f59242
vamin9 <vamin9@gatech.edu>
2023-07-10 13:41:41 -0400

    updated href navbar

--- portfolio-react-app/src/components/Navbar.jsx
+++ portfolio-react-app/src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
 import { HiOutlineMail } from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 import Logo from '../assets/logo.png'
+import { Link } from 'react-scroll'
+import resume from '../assets/resume.pdf'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
@@ -15,11 +17,31 @@ const Navbar = () => {
 
         {/* menu */}
         <ul className='hidden md:flex'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Skills</li>
-            <li>Work</li>
-            <li>Contact</li>
+            <li>
+                <Link to="home" smooth={true} duration={500}>
+                    Home
+                </Link>
+            </li>
+            <li>
+                <Link to="about" smooth={true} duration={500}>
+                    About
+                </Link>
+            </li>
+            <li>
+                <Link to="skills" smooth={true} duration={500}>
+                    Skills
+                </Link>
+            </li>
+            <li>
+                <Link to="work" smooth={true} duration={500}>
+                    Work
+                </Link>
+            </li>
+            <li>
+                <Link to="contact" smooth={true} duration={500}>
+                    Contact
+                </Link>
+            </li>
         </ul>
 
 
@@ -29,23 +51,43 @@ const Navbar = () => {
 
         {/* mobile menu */}
         <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-            <li className='py-6 text-4xl'>Home</li>
-            <li className='py-6 text-4xl'>About</li>
-            <li className='py-6 text-4xl'>Skills</li>
-            <li className='py-6 text-4xl'>Work</li>
-            <li className='py-6 text-4xl'>Contact</li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+                    Home
+                </Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+                    About
+                </Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+                    Skills
+                </Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+                    Work
+                </Link>
+            </li>
+            <li className='py-6 text-4xl'>
+                <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+                    Contact
+                </Link>
+            </li>
         </ul>
 
         {/* social icons */}
         <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
             <ul>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-                    <a className='flex justify-between items-center w-full text-gray-300' href=''>
+                    <a className='flex justify-between items-center w-full text-gray-300' href='https://www.linkedin.com/in/vedant-amin-0b741718a/' target='_blank'>
                         Linkedin <FaLinkedin size={30}/>
                     </a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-                    <a className='flex justify-between items-center w-full text-gray-300' href=''>
+                    <a className='flex justify-between items-center w-full text-gray-300' href='https://github.com/Vedant4824?tab=repositories' target='_blank'>
                         Github <FaGithub size={30}/>
                     </a>
                 </li>
@@ -55,7 +97,7 @@ const Navbar = () => {
                     </a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-                    <a className='flex justify-between items-center w-full text-gray-300' href=''>
+                    <a className='flex justify-between items-center w-full text-gray-300' href={resume} target='_blank' rel="noopener noreferrer">
                         Resume <BsFillPersonLinesFill size={30}/>
                     </a>
                 </li>
