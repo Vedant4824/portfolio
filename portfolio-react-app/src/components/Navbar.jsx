46f06278e66832ae5248e0299d67585a32ef52f3
vamin9 <vamin9@gatech.edu>
2023-09-05 23:40:10 -0400

    update

--- portfolio-react-app/src/components/Navbar.jsx
+++ portfolio-react-app/src/components/Navbar.jsx
@@ -96,7 +96,11 @@ const Navbar = () => {
                         Email <HiOutlineMail size={30}/>
                     </a>
                 </li>
-            
+                <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+                    <a className='flex justify-between items-center w-full text-gray-300' href={resume} target='_blank' rel="noreferrer">
+                        Resume <BsFillPersonLinesFill size={30}/>
+                    </a>
+                </li>
             </ul>
 
         </div>
