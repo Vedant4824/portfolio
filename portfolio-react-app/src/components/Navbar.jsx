2c5952e7ab0b2b7720b8a79ddbabfeeded5116e3
Vedant Amin <vaminjobs@gmail.com>
2023-10-15 14:18:49 -0400

    Updated Resume

--- portfolio-react-app/src/components/Navbar.jsx
+++ portfolio-react-app/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { HiOutlineMail } from 'react-icons/hi'
 import { BsFillPersonLinesFill } from 'react-icons/bs'
 import Logo from '../assets/logo.png'
 import { Link } from 'react-scroll'
-import resume from '../assets/resume.pdf'
+import Vedant_Resume from '../assets/Vedant_Resume.pdf'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false)
@@ -97,7 +97,7 @@ const Navbar = () => {
                     </a>
                 </li>
                 <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-                    <a className='flex justify-between items-center w-full text-gray-300' href={resume} target='_blank' rel="noreferrer">
+                    <a className='flex justify-between items-center w-full text-gray-300' href={Vedant_Resume} target='_blank' rel="noreferrer">
                         Resume <BsFillPersonLinesFill size={30}/>
                     </a>
                 </li>
